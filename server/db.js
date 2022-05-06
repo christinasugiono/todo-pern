e6de9308ca458ad3df8b4a6b1a0b4fe6f41fab70
Christina Sugiono <christina.sugiono95@gmail.com>
2022-05-06 23:03:26 +0800

    routes crud

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: 5432,
+  database: process.env.DB
+});
+
+module.exports = pool;
